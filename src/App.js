303173c88735847c4aef2e023ab415f67d61a619
Akash <na.akash715@gmail.com>
2023-01-29 02:47:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
+      <District name='Khulna' speciality='Bibhag'></District>
+      <District name='Dhaka' speciality='Bibhag'></District>
+    </div>
+  );
+}
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, []);
+  return (
+    <div className='py-5'>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map(post => <Post post={post}></Post>)
+      }
+    </div>
+  );
+}
+
+function Post(props) {
+  return (
+    <div style={{
+      backgroundColor: 'lightgray',
+      margin: '20px', padding: '20px',
+      borderRadius: '20px', border: '4px solid red'
+    }}>
+      <h2>{props.post.title}</h2>
+      <p>{props.post.body}</p>
+    </div>
+  )
+}
+
+
+
+
+
+const districtStyle = {
+  backgroundColor: 'skyblue',
+  padding: '20px',
+  margin: '20px',
+  borderRadius: '20px'
+}
+
+function District(props) {
+  const [power, setPower] = useState(1);
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  }
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.speciality}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower} className='btn btn-outline-primary my-2'>Boost</button>
     </div>
   );
 }
